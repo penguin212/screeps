6015f9c553a7038915784756ae5d550f9f9303da
penguin212 <dhuber06@gmail.com>
2022-09-19 19:58:22 -0700

    support for multiple sources (real)

--- role.harvester.js
+++ role.harvester.js
@@ -4,8 +4,8 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[creep.room.memory.harvester_source]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.room.memory.harvester_source], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
