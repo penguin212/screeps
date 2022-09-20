dad23a60a007ace32d573570be4ed102c2ea60ed
penguin212 <dhuber06@gmail.com>
2022-09-19 20:00:18 -0700

    builder multiple sources

--- role.builder.js
+++ role.builder.js
@@ -22,8 +22,8 @@ var roleBuilder = {
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[creep.room.memory.builder_source]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[creep.room.memory.builder_source], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
