34f3b9ec36cb5b46c537c7dd36468f7303bdf70e
penguin212 <dhuber06@gmail.com>
2022-09-20 20:21:01 -0700

    fix

--- autospawner.js
+++ autospawner.js
@@ -8,9 +8,9 @@ var autospawner = {
         var total_upgraders = Game.spawns[spawner].memory.num_upgraders;
         var total_builders = Game.spawns[spawner].memory.num_builders;
 
-        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester' && c.memory.room == Game.spawns[spawner].room);
-        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader' && c.memory.room == Game.spawns[spawner].room);
-        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.spawns[spawner].room);
+        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester' && c.memory.room.name == Game.spawns[spawner].room);
+        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader' && c.memory.room.name == Game.spawns[spawner].room);
+        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room.name == Game.spawns[spawner].room);
         
         if(total_harvesters > current_harvesters){
             roleHarvester.spawn(spawner);
