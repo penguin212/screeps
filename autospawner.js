76d4ce24f82688dfec217912e62aa0f233b81bb1
penguin212 <dhuber06@gmail.com>
2022-09-20 19:51:24 -0700

    test autospawn

--- autospawner.js
+++ autospawner.js
@@ -4,20 +4,20 @@ var roleBuilder = require('role.builder');
 
 var autospawner = {
     run: function(spawner){
-        var total_harvesters = spawner.memory.num_harvesters
-        var total_upgraders = spawner.memory.num_upgraders
-        var total_builders = spawner.memory.num_builders
+        var total_harvesters = Game.getObjectById(spawner).memory.num_harvesters
+        var total_upgraders = Game.getObjectById(spawner).memory.num_upgraders
+        var total_builders = Game.getObjectById(spawner).memory.num_builders
 
-        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == spawner.room);
-        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == spawner.room);
-        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == spawner.room);
+        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
+        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
+        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
         
         if(total_harvesters < current_harvesters){
-            roleHarvester.spawn(spawner);
+            roleHarvester.spawn(Game.getObjectById(spawner));
         } else if(total_upgraders < current_upgraders){
-            roleUpgrader.spawn(spawner);
+            roleUpgrader.spawn(Game.getObjectById(spawner));
         } else if(total_builders < current_builders){
-            roleBuilder.spawn(spawner);
+            roleBuilder.spawn(Game.getObjectById(spawner));
         }
     }
 }
--- main.js
+++ main.js
@@ -34,7 +34,7 @@ module.exports.loop = function () {
     }
 
     for(var spawner in Game.spawns) {
-        //autospawner.run(spawner);
+        autospawner.run(spawner.id);
     }
 
 }
