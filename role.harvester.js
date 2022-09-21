484d3b98904d81022755ea75d95e5b0f387b5766
penguin212 <dhuber06@gmail.com>
2022-09-20 23:35:58 -0700

    idle harvesters go to container instead of source

--- role.harvester.js
+++ role.harvester.js
@@ -21,6 +21,15 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            }else{
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER);
+                    }
+                });
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
     },
