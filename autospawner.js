4807f6c429b764d3673d3bed9012cc3a31041247
penguin212 <dhuber06@gmail.com>
2022-09-20 19:54:15 -0700

    fix pt2

--- autospawner.js
+++ autospawner.js
@@ -5,20 +5,20 @@ var roleBuilder = require('role.builder');
 var autospawner = {
     run: function(spawner){
         console.log(spawner);
-        var total_harvesters = Game.getObjectById(spawner).memory.num_harvesters;
-        var total_upgraders = Game.getObjectById(spawner).memory.num_upgraders;
-        var total_builders = Game.getObjectById(spawner).memory.num_builders;
+        var total_harvesters = Game.spawns[spawner].memory.num_harvesters;
+        var total_upgraders = Game.spawns[spawner].memory.num_upgraders;
+        var total_builders = Game.spawns[spawner].memory.num_builders;
 
-        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
-        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
-        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.getObjectById(spawner).room);
+        var current_harvesters = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.spawns[spawner].room);
+        var current_upgraders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.spawns[spawner].room);
+        var current_builders = _.sum(Game.creeps, (c) => c.memory.role == 'builder' && c.memory.room == Game.spawns[spawner].room);
         
         if(total_harvesters < current_harvesters){
-            roleHarvester.spawn(Game.getObjectById(spawner));
+            roleHarvester.spawn(Game.spawns[spawner]);
         } else if(total_upgraders < current_upgraders){
-            roleUpgrader.spawn(Game.getObjectById(spawner));
+            roleUpgrader.spawn(Game.spawns[spawner]);
         } else if(total_builders < current_builders){
-            roleBuilder.spawn(Game.getObjectById(spawner));
+            roleBuilder.spawn(Game.spawns[spawner]);
         }
     }
 }
